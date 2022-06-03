2a94b350d139ceff2f971f3e822f78ce9dbd366c
PseudoPolymath <benjamin.wyland@gmail.com>
2022-06-02 22:21:48 -0700

    Fixed Button, Need to Fix Servo
    Fixed Comparative Operators (lol). Button works now. Not sure how to make servo go back and forth, seems to get stuck after initial press.

--- Hourglass/Hourglass.ino
+++ Hourglass/Hourglass.ino
@@ -1,31 +1,41 @@
 #include <Servo.h> 
 
-Servo hourglass_servo;
+Servo servo;
 bool toggle = false;
 int button = 0;
+int pos = 0;
+
+int servoPin = 11;
+int ledPin = 12;
+int buttonPin = 2;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(2, INPUT);
-  hourglass_servo.attach(13);
-  digitalWrite(13, LOW);
+  pinMode(servoPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  servo.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (toggle = true) {
-    hourglass_servo.write(180);
-    digitalWrite(13, HIGH);
+  if (toggle == true) {
+    pos = servo.read();
+    if (pos = 180) {
+      servo.write(0);
+    } else {
+      servo.write(180);
+    }
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
   }
-  if (toggle = false) {
-    digitalWrite(13, HIGH);
-  }
+
   for (int i = 0; i < 450; i++) {
-    button = digitalRead(3);
-    if (button = HIGH) {
+    button = digitalRead(buttonPin);
+    if (button == HIGH) {
       toggle = !toggle;
+      break;
     }
     delay(100);
   }
